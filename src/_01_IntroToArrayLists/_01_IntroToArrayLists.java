14bbf0f4fa238e9c380732a92652eee9f0ecdd67
kaylarwright <kayla.wright@gompersstudent.org>
2019-08-19 17:45:28 -0700

    hi

--- src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
+++ src/_01_IntroToArrayLists/_01_IntroToArrayLists.java
@@ -6,16 +6,29 @@ public class _01_IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList class
-
+		ArrayList<String> num = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		num.add("one");
+		num.add("two");
+		num.add("three");
+		num.add("four");
+		num.add("five");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = 0; i < num.size(); i++) {
+			String value = num.get(i);
+			System.out.println(value);
+		}
 		
 		//4. Print all the Strings using a for-each loop
+		for (String Value : num) {
+			System.out.println(Value);
+		}
 		
 		//5. Print only the even numbered elements in the list.
-		
+		for(int i = 0; i < num.size(); i+=2) {
+			String value = num.get(i);
+			System.out.println(value);
+		}
 		//6. Print all the Strings in reverse order.
 		
 		//7. Print only the Strings that have the letter 'e' in them.
