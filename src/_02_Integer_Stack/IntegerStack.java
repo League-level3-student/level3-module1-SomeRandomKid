a40a31541f53c2b6578a6f24bed47c4afae037df
ilabstaff <ilabstaff@172.16.116.73>
2019-11-04 17:42:41 -0800

    hi

--- src/_02_Integer_Stack/IntegerStack.java
+++ src/_02_Integer_Stack/IntegerStack.java
@@ -1,50 +1,57 @@
 package _02_Integer_Stack;
 
+import java.util.ArrayList;
+
 public class IntegerStack {
 	//1. create a private array of integers
-	
+	private int[] num;
 	//2. complete the constructor by initializing the member array
 	//   to a new array of length 0. This prevents null pointer exceptions.
 	public IntegerStack() {
+		num = new int[0];
 	}
 	
 	//3. Complete the steps in the push method.
 	public void push(int v) {
 		//A. create a new array that is one element larger than the member array
-		
+		int[] push = new int[num.length + 1];
 		//B. set the last element of the new array equal to the integer passed into the method
-		
+		push[push.length-1] = v;
 		//C. iterate through the member array and copy all the elements into the new array
-		
+		for (int i = 0; i < num.length; i++) {
+			push[i] = num[i];
+		}
 		//D. set the member array equal to the new array.
-		
+		num = push;
 	}
 	
 	//4. Complete the steps in the pop method.
 	public int pop() {
 		//A. create an integer variable and initialize it to the
 		//   last element of the member array.
-		
+		int var = num.length - 1;
 		//B. create a new array that is one element smaller than the member array
-		
+		int[] pop = new int[num.length - 1];
 		//C. iterate through the new array and copy every element from the
 		//   member array to the new array
-		
+		for (int i = 0; i < pop.length; i++) {
+			pop[i] = num[i];
+		}
 		//D. set the member array equal to the new array
-		
+		num = pop;
 		//E. return the variable you created in step A
-		return 0;
+		return var;
 	}
 	
 	//5. Complete the clear method to set the
 	//   member array to a new array of length 0
 	public void clear() {
-		
+		num = new int[0];
 	}
 	
 	//6. Complete the size method to return 
 	//   the length of the member array
 	public int size() {
-		return 0;
+		return num.length;
 	}
 }
--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,30 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack<Double> num = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for (int i = 0; i < 100; i++) {
+			Random randNum = new Random();
+		double numRandom = randNum.nextDouble();
+		double rand = numRandom * 100;
+			num.push(rand);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
-		
+		 String output1 = JOptionPane.showInputDialog("Pick a number.");
+		 String output2 = JOptionPane.showInputDialog("Pick another number.");
+		 int num1 = Integer.parseInt(output1);
+		 int num2 = Integer.parseInt(output2);
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		for (int i = 0; i < 100; i++) {
+		double save = num.pop();
+			if (num1 < save && save < num2) {
+				System.out.println(save);
+			}
+			else if (num2 < save && save < num1) {
+				System.out.println(save);
+			}
+		}
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
