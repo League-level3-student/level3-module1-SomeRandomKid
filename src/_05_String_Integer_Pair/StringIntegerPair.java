77c0c2c1bb9343ffb97cc7e9bd844614f9bb8665
ilabstaff <ilabstaff@172.16.116.128>
2020-03-09 17:42:08 -0700

    hi

--- src/_05_String_Integer_Pair/StringIntegerPair.java
+++ src/_05_String_Integer_Pair/StringIntegerPair.java
@@ -1,12 +1,16 @@
 package _05_String_Integer_Pair;
 
+import java.util.ArrayList;
+
 public class StringIntegerPair {
 	//1. create a private array of Strings called keys. Don't initialize it.
-
+ private String[] keys;
 	//2. create a private array of integers called values.
-	
+	private int[] values;
 	StringIntegerPair(){
 		//3. initialize both member arrays to a length of 0
+		keys = new String[0];
+		values = new int[0]; 
 	}
 	
 	// 4. Complete the steps in the put method
@@ -14,15 +18,20 @@ public class StringIntegerPair {
 		//A. iterate through the keys. If you find a key that matches
 		//   the passed in String, set the value at that location to the
 		//   passed in value and return from the method.
-		
+		for (int i = 0; i < keys.length; i++) {
+			if (keys[i] == key) {
+				values[i] = value;
+				return;
+			}
+		}
 		//B. create a String array that is one element longer than the keys
-		
+		String[] keys2 = new String[keys.length + 1];
 		//C. create an integer array that is one element longer than values
-		
+		int[] value2 = new int[values.length + 1];
 		//D. set the last element of the new String array to the passed in key
-		
+		keys2[keys2.length] = key;
 		//E. set the last element of the new int array to the passed in value
-		
+		value2[value2.length] = value;
 		//F. iterate through the keys and values and copy the elements to the new arrays
 		
 		//G. Set the keys and values arrays equal to the new arrays created in steps B and C.
